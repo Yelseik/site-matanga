56314d663c1c458777411d5756ab73285c539610
Yelseik <o_teu_email_do_github@example.com>
2026-06-08 09:13:01 +0100

    Fix: separando o service worker do index.html

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'area51-v2';
+const URLS = [
+  './',
+  './index.html',
+  './sw.js',
+  'https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.8.0/firebase-database-compat.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(URLS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      if (res) return res;
+      return fetch(e.request).then(response => {
+        let clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return response;
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE) return caches.delete(key);
+      })
+    ))
+  );
+});
