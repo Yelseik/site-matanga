5a897a7df51b4997d816fb069ebc6f7e2d9855fc
Yelseik <o_teu_email_do_github@example.com>
2026-06-08 09:23:41 +0100

    Fix: final do index e sw.js isolado

--- sw.js
+++ sw.js
@@ -1,35 +1,7 @@
-const CACHE = 'area51-v2';
-const URLS = [
-  './',
-  './index.html',
-  './sw.js',
-  'https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/10.8.0/firebase-database-compat.js'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(URLS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      if (res) return res;
-      return fetch(e.request).then(response => {
-        let clone = response.clone();
-        caches.open(CACHE).then(cache => cache.put(e.request, clone));
-        return response;
-      });
-    }).catch(() => caches.match('./index.html'))
-  );
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (key !== CACHE) return caches.delete(key);
-      })
-    ))
-  );
-});
+cat > sw.js << 'SWEOF'
+const CACHE='area51-v2';
+const URLS=['/index.html','/sw.js','https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js','https://www.gstatic.com/firebasejs/10.8.0/firebase-database-compat.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(URLS).catch(()=>{}))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return res;}).catch(()=>caches.match('/index.html')))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+SWEOF
